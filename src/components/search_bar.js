efcc9c87f1f6870876ca5dce1f16e4a97242afd5
pedrom40 <pedro.morin1980@gmail.com>
2017-07-21 16:12:03 -0500

    more using fat arrow, learning about state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component { // class based component
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
 
