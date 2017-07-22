a8fd850e2b814f20e0ae28933f827f2c68d4f716
pedrom40 <pedro.morin1980@gmail.com>
2017-07-22 07:56:21 -0500

    add video player

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyABE1JvOgmJwKdcITqnniB4C0btCwBFU00';
 
 class App extends Component {
@@ -20,11 +21,12 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
   }
-  
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
